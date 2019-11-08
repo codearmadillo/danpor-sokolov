74a68208aabaed42f78d4fd0d12aa35fea73f1c6
jiriKralovec <jrl@scancommerce.dk>
2019-11-08 17:02:32 +0100

    Reworks frontpage grid

--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -26,51 +26,49 @@
     </div>
 </div>
 <div class="page__content">
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <h2 style="text-align: center;">
-                <?php echo get_field('services', $post->ID)["headline"]; ?>
-                </h2>
-                <p style="text-align: center;">
-                <?php echo get_field('services', $post->ID)["body"]; ?>
-                </p>
-            </div>
-            <?php
-            $servicesPage = get_pages(array(
-                'post_type'     =>'page',
-                'meta_key'      =>'_wp_page_template',
-                'hierarchical'  =>0,
-                'meta_value'    => 'page-templates/services.php'
-            ))[0];
+  <div class="container">
+    <div class="services-grid">
+      <h2 class="services-header" style="text-align: center;">
+      <?php echo get_field('services', $post->ID)["headline"]; ?>
+      </h2>
+      <p class="services-desc" style="text-align: center;">
+      <?php echo get_field('services', $post->ID)["body"]; ?>
+      </p>
+      <?php
+      $servicesPage = get_pages(array(
+        'post_type'     =>'page',
+        'meta_key'      =>'_wp_page_template',
+        'hierarchical'  =>0,
+        'meta_value'    => 'page-templates/services.php'
+      ))[0];
 
-            $args = array(
-                'post_type'      => 'page',
-                'posts_per_page' => -1,
-                'post_parent'    => $servicesPage->ID,
-                'order'          => 'ASC',
-                'orderby'        => 'menu_order'
-            );
+      $args = array(
+        'post_type'      => 'page',
+        'posts_per_page' => -1,
+        'post_parent'    => $servicesPage->ID,
+        'order'          => 'ASC',
+        'orderby'        => 'menu_order'
+      );
 
-            $query = new WP_Query($args);
-            if($query->have_posts()){
-                while($query->have_posts()){
-                    $query->the_post();
-                    $url = get_permalink($post->ID);
-                    $meta = get_field('snippet');
-                    echo '
-                    <div class="services__card col-xs-12 col-sm-6 col-md-4 col-lg-3" style="position: relative;">
-                        <span class="services__card-icon"><i class="fas '.$meta["icon"].' fa-2x"></i></span>
-                        <h2>'.$meta["headline"].'</h2>
-                        <p>'.$meta["desc"].'</p>
-                        <a href="'.$url.'" title="'.$meta["headline"].'" class="btn-default btn--fill services__card-button">Zobrazit více</a>
-                    </div>
-                    ';
-                };
-            };
-            ?>
-        </div>
+      $query = new WP_Query($args);
+      if($query->have_posts()){
+        while($query->have_posts()){
+          $query->the_post();
+          $url = get_permalink($post->ID);
+          $meta = get_field('snippet');
+          echo '
+          <article class="services-item services__card" style="position: relative;">
+            <span class="services__card-icon"><i class="fas '.$meta["icon"].' fa-2x"></i></span>
+            <h2>'.$meta["headline"].'</h2>
+            <p>'.$meta["desc"].'</p>
+            <a href="'.$url.'" title="'.$meta["headline"].'" class="btn-default btn--fill services__card-button">Zobrazit více</a>
+          </article>
+          ';
+        };
+      };
+      ?>
     </div>
+  </div>
 </div>
 
 <?php
